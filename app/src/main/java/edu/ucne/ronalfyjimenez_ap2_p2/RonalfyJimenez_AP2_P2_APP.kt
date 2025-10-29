5bbe987e46a33f5f50e74e32f269342a536d5b9c
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2025-10-29 19:52:03 -0400

    se implemento hilt y permiso de internet

---
+++ app/src/main/java/edu/ucne/ronalfyjimenez_ap2_p2/RonalfyJimenez_AP2_P2_APP.kt
@@ -0,0 +1,7 @@
+package edu.ucne.ronalfyjimenez_ap2_p2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RonalfyJimenez_AP2_P2_APP : Application()
